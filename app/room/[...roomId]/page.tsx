9ad6e242d7a732a2b3b88c3f1d0743ae496dbb56
nitzanto <nitzan16001@gmail.com>
2023-11-16 20:20:39 +0200

    * Code editor component added

---
+++ app/room/[...roomId]/page.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import React, { useState } from 'react';
+import SplitPane, { Pane } from 'split-pane-react';
+import 'split-pane-react/esm/themes/default.css';
+import CodeEditor from "@/components/Workspace/codeEditor/CodeEditor";
+
+const Room = () => {
+    const [sizes, setSizes] = useState([100, '30%', 'auto']);
+
+    const layoutCSS = {
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: '#2d2d2d', // Dark gray background color
+        color: '#fff', // Text color
+    };
+
+    const sashRender = (index: any, active: any) => {
+      return (
+        <div
+          className={`bg-${active ? "gray-700" : "gray-600"} h-full w-2`}
+          style={{ cursor: "col-resize" }}
+        />
+      );
+    };
+
+    return (
+        <div className="h-screen bg-black">
+            <SplitPane
+                split='vertical'
+                sizes={sizes}
+                onChange={setSizes}
+                sashRender={sashRender}
+            >
+                <Pane>
+                    <div className="flex items-center justify-center h-full bg-gray-800">
+                        {/* Explanation content goes here */}
+                        Explanation Pane
+                    </div>
+                </Pane>
+                <Pane>
+                    <div className="flex items-center justify-center h-full bg-gray-700">
+                        {/* CodeEditor component goes here */}
+                        <CodeEditor />
+                    </div>
+                </Pane>
+            </SplitPane>
+        </div>
+    );
+};
+
+export default Room;
--- components/Workspace/codeEditor/CodeEditor.tsx
+++ components/Workspace/codeEditor/CodeEditor.tsx
@@ -1,17 +1,45 @@
-import React from "react";
-import { useState, useRef } from "react";
+import React, { useState } from "react";
 import Editor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-  return <div className="code-editor">
-    <Editor
-      height="100vh"
-      width="100%"
-      theme="vs-dark"
-      defaultLanguage={"python"}
-      defaultValue={"pytohn.py"}
-      />
-  </div>;
+  const [language, setLanguage] = useState("python");
+  const [value, setValue] = useState("python");
+
+  const handleLanguageChange = (newLanguage: React.SetStateAction<string>) => {
+    setLanguage(newLanguage);
+    setValue(newLanguage);
+  };
+
+  return (
+    <div className="flex flex-col w-full h-full">
+      {/* Language selection bar */}
+      <div className="pl-2 border border-solid border-gray-700 bg-neutral-800 cursor-auto flex item-center">
+        <div>
+          <div className="text-gray-300 font-bold mb-1">Text Editor</div>
+          <div className="text-xs text-gray-400 mb-1">
+            You can edit your code here
+          </div>
+        </div>
+        <select
+          className="p-2 m-2 bg-neutral-500 text-gray-800 text-sm rounded-b ml-auto"
+          onChange={(e) => handleLanguageChange(e.target.value)}
+        >
+          <option value="python">Python</option>
+          <option value="javascript">JavaScript</option>
+        </select>
+      </div>
+      {/* Code editor section */}
+      <div className="flex-1 bg-gray-100 rounded-b-lg shadow-lg">
+        <Editor
+          height="calc(100vh - 40px)" // Adjust the height to leave space for the top bar
+          width="100%"
+          theme="vs-dark"
+          language={language}
+          value={`// Start coding in ${value}\n`}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CodeEditor;
